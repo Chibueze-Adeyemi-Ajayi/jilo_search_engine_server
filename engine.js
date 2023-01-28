1fac11e7e8bec905cf69bf8e4c911f7e70d87279
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-28 23:22:27 +0100

    App Server

--- engine.js
+++ engine.js
@@ -56,7 +56,7 @@ const middleware_direct = (req, res, next) => {
         next();
     });
 };
-//direct to google's al
+//direct to google's a
 app.post('/search/direct', middleware_direct, (req, res) => {
     console.log(req.query);
     res.send(req.result);
